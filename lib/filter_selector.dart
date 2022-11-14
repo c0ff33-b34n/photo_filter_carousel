8e18764fc9552297bea1c59dae0dc9a75d9b66cf
Stuart Bowes <stuartbowes2000@gmail.com>
2022-11-14 14:47:50 +0000

    Add background shadow gradient and selection ring to FilterSelector Widget

--- lib/filter_selector.dart
+++ lib/filter_selector.dart
@@ -1,15 +1,71 @@
 import 'package:flutter/material.dart';
 
 class FilterSelector extends StatefulWidget {
-  const FilterSelector({super.key});
+  const FilterSelector(
+      {super.key, this.padding = const EdgeInsets.symmetric(vertical: 24.0)});
 
+  final EdgeInsets padding;
   @override
   State<FilterSelector> createState() => _FilterSelectorState();
 }
 
 class _FilterSelectorState extends State<FilterSelector> {
+  static const _filtersPerScreen = 5;
+  static const _viewportFractionPerItem = 1.0 / _filtersPerScreen;
+
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final itemSize = constraints.maxWidth * _viewportFractionPerItem;
+
+        return Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            _buildShadowGradient(itemSize),
+            _buildSelectionRing(itemSize),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildShadowGradient(double itemSize) {
+    return SizedBox(
+      height: itemSize * 2 + widget.padding.vertical,
+      child: const DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.transparent,
+              Colors.black,
+            ],
+          ),
+        ),
+        child: SizedBox.expand(),
+      ),
+    );
+  }
+
+  Widget _buildSelectionRing(double itemSize) {
+    return IgnorePointer(
+      child: Padding(
+        padding: widget.padding,
+        child: SizedBox(
+          width: itemSize,
+          height: itemSize,
+          child: const DecoratedBox(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.fromBorderSide(
+                BorderSide(width: 6.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
