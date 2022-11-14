1175173745ad3439d616c693b7da2038ddfd92d1
Stuart Bowes <stuartbowes2000@gmail.com>
2022-11-14 15:20:31 +0000

    Change opacity and size of FilterItems based on distance from selected item

--- lib/filter_selector.dart
+++ lib/filter_selector.dart
@@ -89,9 +89,54 @@ class _FilterSelectorState extends State<FilterSelector> {
         itemCount: widget.filters.length,
         itemBuilder: (context, index) {
           return Center(
-            child: FilterItem(
-              color: itemColor(index),
-              onFilterSelected: () {},
+            child: AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                if (!_controller.hasClients ||
+                    !_controller.position.hasContentDimensions) {
+                  // The PageViewController isn’t connected to the
+                  // PageView widget yet. Return an empty box.
+                  return const SizedBox();
+                }
+
+                // The integer index of the current page,
+                // 0, 1, 2, 3, and so on
+                final selectedIndex = _controller.page!.roundToDouble();
+
+                // The fractional amount that the current filter
+                // is dragged to the left or right, for example, 0.25 when
+                // the current filter is dragged 25% to the left.
+                final pageScrollAmount = _controller.page! - selectedIndex;
+
+                // The page-distance of a filter just before it
+                // moves off-screen.
+                const maxScrollDistance = _filtersPerScreen / 2;
+
+                // The page-distance of this filter item from the
+                // currently selected filter item.
+                final pageDistanceFromSelected =
+                    (selectedIndex - index + pageScrollAmount).abs();
+
+                // The distance of this filter item from the
+                // center of the carousel as a percentage, that is, where the selector
+                // ring sits.
+                final percentFromCenter =
+                    1.0 - pageDistanceFromSelected / maxScrollDistance;
+
+                final itemScale = 0.5 + (percentFromCenter * 0.5);
+                final opacity = 0.25 + (percentFromCenter * 0.75);
+
+                return Transform.scale(
+                  scale: itemScale,
+                  child: Opacity(
+                    opacity: opacity,
+                    child: FilterItem(
+                      color: itemColor(index),
+                      onFilterSelected: () => () {},
+                    ),
+                  ),
+                );
+              },
             ),
           );
         },
